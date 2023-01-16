d012e290f133a961f97871d65f88fa22b4ec6ae3
leonardvsc <leonardoworld@outlook.com>
2023-01-16 00:23:39 -0300

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,6 @@ if(navClose){
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
-
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
@@ -35,7 +34,23 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0 ; i < skillsContent.length ; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass == 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
